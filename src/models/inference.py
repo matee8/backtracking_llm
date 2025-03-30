fix(inference): remove unnecessary dim parameter in softmax

src/models/inference.py
@@ -55,7 +55,7 @@ def run_inference(
             for _ in range(max_length - input_ids.shape[-1]):
                 outputs: CausalLMOutputWithCrossAttentions = model(input_ids)
                 logits: Tensor = outputs.logits[:, -1, :]
-                probabilites: Tensor = F.softmax(logits, dim=-1)
+                probabilites: Tensor = F.softmax(logits)
 
                 top_k_probs: Tensor
                 top_k_ids: Tensor
b6caf6dfbc93d5a93c2aba2f904fdb6548b29020
matee8 <graves-bluff-pesky@duck.com>
2025-04-18 23:39:00 +0200

    refactor(decision): change type hinting to newer way

--- backtracking_llm/models/decision.py
+++ backtracking_llm/models/decision.py
@@ -3,16 +3,16 @@ import typing
 import torch
 
 BacktrackingDecisionFunctionType = typing.Callable[
-    [torch.Tensor, torch.Tensor, int, typing.Dict[str, typing.Any]],
-    typing.Tuple[bool, int]]
+    [torch.Tensor, torch.Tensor, int, dict[str, typing.Any]],
+    tuple[bool, int]]
 
 
 def simple_threshold_decision(
     top_k_logits_seq: torch.Tensor,
     top_k_probabilites_seq: torch.Tensor,
     chosen_token_relative_idx: int,
-    config: typing.Dict[str, typing.Any],
-) -> typing.Tuple[bool, int]:
+    config: dict[str, typing.Any],
+) -> tuple[bool, int]:
     BACKTRACK_TOKEN_COUNT: int = 1
     chosen_prob: float = (
         top_k_probabilites_seq[chosen_token_relative_idx].item())

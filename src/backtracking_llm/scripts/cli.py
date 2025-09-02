7f770c91ae8e6e4dcc6c2afbfa31538b389cf767
matee8 <graves-bluff-pesky@duck.com>
2025-09-02 22:13:31 +0200

    feat(scripts): add cli script

---
+++ src/backtracking_llm/scripts/cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Provides an interactive command-line interface for chatting with a model
+using the backtracking_llm library.
+"""
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
